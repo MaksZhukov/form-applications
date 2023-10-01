6b16077d0f34ef17922b7f5e1e43752645be74a3
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-10-02 00:01:48 +0300

    Fixed

--- src/db/comment/model.ts
+++ src/db/comment/model.ts
@@ -1,8 +1,9 @@
-import { BelongsTo, BelongsToManyAddAssociationMixin, Model } from 'sequelize';
+import { BelongsToManyAddAssociationMixin, Model } from 'sequelize';
 import { CommentAttributes, CommentAttributesCreation } from './types';
 import { ApplicationModel } from '../application/model';
-import { UserModel } from '../users/model';
+import { ApplicationInternalModel } from '../applicationInternal/model';
 
 export class CommentModel extends Model<CommentAttributes, CommentAttributesCreation> {
 	declare addApplication: BelongsToManyAddAssociationMixin<ApplicationModel, number>;
+	declare addInternalApplication: BelongsToManyAddAssociationMixin<ApplicationInternalModel, number>;
 }
