813ab1d2fee2a4a8bee864f1edbfb37bfb3f1c62
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-10-22 20:22:46 +0300

    Returned

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,22 @@
+import { initialize } from '@/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+	// IT NEEDS for BUILD ''
+	const token = request.cookies.get('token')?.value;
+	if (!token) {
+		return new NextResponse('no token', { status: 401 });
+	}
+	const { UserModel } = await initialize();
+	const userData = (
+		await UserModel.findOne({
+			where: { token },
+			attributes: ['id', 'email', 'role', 'departmentName', 'name'],
+			include: 'organization'
+		})
+	)?.toJSON();
+	if (!userData) {
+		return new NextResponse('wrong token', { status: 401 });
+	}
+	return NextResponse.json({ data: userData });
+}
