fcaedf41a9b1d805ddeb8c1ad4e92744e3379044
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-10-23 23:23:15 +0300

    Finished ui for kind of work

--- src/app/components/Application/LaborCosts/ModalAddLaborCosts.tsx
+++ src/app/components/Application/LaborCosts/ModalAddLaborCosts.tsx
@@ -2,7 +2,7 @@ import { fetchUser } from '@/app/api/user';
 import { getLoginTime } from '@/app/localStorage';
 import { Button, Typography } from '@material-tailwind/react';
 import { useQuery } from '@tanstack/react-query';
-import { FC, FormEventHandler } from 'react';
+import { ChangeEvent, ChangeEventHandler, FC, FormEventHandler, useState } from 'react';
 
 interface Props {
 	onSubmit: FormEventHandler<HTMLFormElement>;
@@ -20,15 +20,35 @@ const HOURS_OF_WORK = [
 	'5 часов',
 	'6 часов',
 	'7 часов',
-	'8 часов',
+	'8 часов'
 ];
 
+const KIND_OF_WORK = ['Разработка'];
+
 const ModalAddLaborCosts: FC<Props> = ({ onSubmit, onCancel }) => {
+	const [isAddingKindOfWork, setIsAddingKindOfWork] = useState<boolean>(false);
+	const [kindOfWork, setKindOfWork] = useState<string>('');
 	const { data: userData } = useQuery(['user', getLoginTime()], {
 		staleTime: Infinity,
 		retry: 0,
-		queryFn: fetchUser,
+		queryFn: fetchUser
 	});
+
+	const handleClickAddKindOfWork = () => {
+		setIsAddingKindOfWork(!isAddingKindOfWork);
+	};
+
+	const handleChangeInputKindOfWork: ChangeEventHandler<HTMLInputElement> = (event) => {
+		setKindOfWork(event.target.value);
+	};
+
+	const handleClickSaveKindOfWork = () => {
+		if (kindOfWork.length > 0) {
+			setKindOfWork('');
+			setIsAddingKindOfWork(false);
+		}
+	};
+
 	return (
 		<>
 			<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
@@ -36,15 +56,14 @@ const ModalAddLaborCosts: FC<Props> = ({ onSubmit, onCancel }) => {
 					{/*content*/}
 					<form
 						onSubmit={onSubmit}
-						className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'
-					>
+						className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
 						{/*header*/}
 						<div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
 							<h3 className='text-3xl font-semibold'>Добавление Трудозатрат</h3>
 						</div>
 						{/*body*/}
 						<div className='relative p-6 flex-auto'>
-							<div>
+							<div className='mb-2'>
 								<Typography>Дата</Typography>
 								<input
 									type='text'
@@ -54,30 +73,58 @@ const ModalAddLaborCosts: FC<Props> = ({ onSubmit, onCancel }) => {
 									defaultValue={new Date().toLocaleDateString()}
 								/>
 							</div>
-							<div>
+							<div className='mb-2'>
 								<Typography>Сотрудник</Typography>
 								<input
 									type='text'
 									className='flex-1 border border-gray-300 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-1 focus:ring-accent focus:outline-none'
 									name='employee'
+									readOnly
 									required
-									defaultChecked={userData?.data?.name || ''}
+									defaultValue={userData?.data?.name || ''}
 								/>
 							</div>
-							<div>
-								<Typography>Email</Typography>
-								<input
-									type='text'
-									className='flex-1 border border-gray-300 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-1 focus:ring-accent focus:outline-none'
-									name='email'
-									required
-								/>
-							</div>
-							<div>
-								<Typography>Количество часов</Typography>
+							<div className='mb-2'>
+								<div className='flex justify-between mb-1'>
+									<Typography>Вид работы</Typography>
+									<Button
+										size='sm'
+										onClick={handleClickAddKindOfWork}
+										className='px-2 py-1 border-accent text-accent'
+										variant='outlined'>
+										{isAddingKindOfWork ? 'Скрыть' : 'Добавить'}
+									</Button>
+								</div>
+								{isAddingKindOfWork && (
+									<div className='flex gap-5 mb-2'>
+										<input
+											value={kindOfWork}
+											onChange={handleChangeInputKindOfWork}
+											type='text'
+											className='flex-1 border border-gray-300 text-sm rounded-lg block w-full px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-1 focus:ring-accent focus:outline-none'
+										/>
+										<Button
+											size='sm'
+											onClick={handleClickSaveKindOfWork}
+											className='px-2 py-1 border-accent text-accent'
+											variant='outlined'>
+											Сохранить
+										</Button>
+									</div>
+								)}
 								<select
-									className={`font-normal border border-gray-300 text-sm rounded-lg block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-1 focus:ring-accent focus:outline-none`}
-								>
+									className={`font-normal w-full h-8 border border-gray-300 text-sm rounded-lg block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-1 focus:ring-accent focus:outline-none`}>
+									{KIND_OF_WORK.map((item) => (
+										<option key={item} value={item}>
+											{item}
+										</option>
+									))}
+								</select>
+							</div>
+							<div className='mb-2'>
+								<Typography>Количество затраченого времени</Typography>
+								<select
+									className={`font-normal w-full h-8 border border-gray-300 text-sm rounded-lg block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-1 focus:ring-accent focus:outline-none`}>
 									{HOURS_OF_WORK.map((item) => (
 										<option key={item} value={item}>
 											{item}
@@ -88,10 +135,18 @@ const ModalAddLaborCosts: FC<Props> = ({ onSubmit, onCancel }) => {
 						</div>
 						{/*footer*/}
 						<div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
-							<Button onClick={onCancel} size='sm' className='ml-1 p-2 border-accent text-accent' variant='outlined'>
+							<Button
+								onClick={onCancel}
+								size='sm'
+								className='ml-1 p-2 border-accent text-accent'
+								variant='outlined'>
 								Отмена
 							</Button>
-							<Button type='submit' size='sm' className='ml-1 p-2 border-accent text-accent' variant='outlined'>
+							<Button
+								type='submit'
+								size='sm'
+								className='ml-1 p-2 border-accent text-accent'
+								variant='outlined'>
 								Добавить
 							</Button>
 						</div>
