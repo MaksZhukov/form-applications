fea8f25b22e1e3b877a58c1d9c192aad91b81347
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-11-26 14:04:51 +0300

    Added total for labor costs

--- src/app/components/Application/LaborCosts/LaborCostsTable.tsx
+++ src/app/components/Application/LaborCosts/LaborCostsTable.tsx
@@ -1,6 +1,7 @@
 import { LaborCostsAttributes } from '@/db/laborCosts/types';
 import { Typography } from '@material-tailwind/react';
 import { FC } from 'react';
+import { MAP_HOURS_OF_WORK_TO_VALUES } from './constants';
 
 const TABLE_HEAD = [
 	{ name: 'Дата', width: 100 },
@@ -15,64 +16,71 @@ interface Props {
 }
 
 const LaborCostsTable: FC<Props> = ({ data }) => {
+	const total = data.reduce(
+		(prev, curr) => prev + MAP_HOURS_OF_WORK_TO_VALUES[curr.timeSpent as keyof typeof MAP_HOURS_OF_WORK_TO_VALUES],
+		0
+	);
 	return (
-		<div className='overflow-y-auto max-h-[260px]'>
-			<table className='w-full table-auto text-left'>
-				<thead>
-					<tr className='bg-white'>
-						{TABLE_HEAD.map((head) => (
-							<th
-								key={head.name}
-								style={{ width: head.width }}
-								className='bg-white sticky top-0 border-b border-gray-100 p-3'>
-								<Typography variant='small' className='font-bold leading-none opacity-90'>
-									{head.name}
-								</Typography>
-							</th>
-						))}
-					</tr>
-				</thead>
-				<tbody className=''>
-					{data.map((item, index) => {
-						const isLast = index === data.length - 1;
-						const classes = isLast
-							? 'w-full p-3 align-baseline'
-							: 'w-full p-3 border-b border-accent align-baseline';
-						return (
-							<tr key={item.id}>
-								<td className={classes} style={{ width: 100 }}>
-									<Typography variant='small' className='font-normal'>
-										{item.date}
+		<>
+			<div className='overflow-y-auto max-h-[236px]'>
+				<table className='w-full table-auto text-left'>
+					<thead>
+						<tr className='bg-white'>
+							{TABLE_HEAD.map((head) => (
+								<th
+									key={head.name}
+									style={{ width: head.width }}
+									className='bg-white sticky top-0 border-b border-gray-100 p-3'>
+									<Typography variant='small' className='font-bold leading-none opacity-90'>
+										{head.name}
 									</Typography>
-								</td>
-								<td className={classes + ' max-w-xs'} style={{ width: 150 }}>
-									<Typography variant='small' className='font-normal'>
-										{item.employee?.name}
-									</Typography>
-								</td>
+								</th>
+							))}
+						</tr>
+					</thead>
+					<tbody className=''>
+						{data.map((item, index) => {
+							const isLast = index === data.length - 1;
+							const classes = isLast
+								? 'w-full p-3 align-baseline'
+								: 'w-full p-3 border-b border-accent align-baseline';
+							return (
+								<tr key={item.id}>
+									<td className={classes} style={{ width: 100 }}>
+										<Typography variant='small' className='font-normal'>
+											{item.date}
+										</Typography>
+									</td>
+									<td className={classes + ' max-w-xs'} style={{ width: 150 }}>
+										<Typography variant='small' className='font-normal'>
+											{item.employee?.name}
+										</Typography>
+									</td>
 
-								<td className={classes + ' max-w-xs'} style={{ width: 150 }}>
-									<Typography variant='small' className='font-normal'>
-										{item.kindsOfWork?.name}
-									</Typography>
-								</td>
+									<td className={classes + ' max-w-xs'} style={{ width: 150 }}>
+										<Typography variant='small' className='font-normal'>
+											{item.kindsOfWork?.name}
+										</Typography>
+									</td>
 
-								<td className={classes} style={{ width: 100 }}>
-									<Typography variant='small' className='font-normal'>
-										{item.timeSpent}
-									</Typography>
-								</td>
-								<td className={classes}>
-									<Typography variant='small' className='font-normal'>
-										{item.description}
-									</Typography>
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-		</div>
+									<td className={classes} style={{ width: 100 }}>
+										<Typography variant='small' className='font-normal'>
+											{item.timeSpent}
+										</Typography>
+									</td>
+									<td className={classes}>
+										<Typography variant='small' className='font-normal'>
+											{item.description}
+										</Typography>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+			{!!total && <div className='pl-3'>Итого: {total} часов</div>}
+		</>
 	);
 };
 
